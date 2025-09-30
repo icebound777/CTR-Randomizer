76c27a0567039025b94a6b612a46db900ce302e2
Icebound777 <frozenblue@gmx.de>
2025-09-30 22:21:20 +0200

    feat: qol mark all mask hints as already seen

---
+++ src/CTRRandomizer_init_profile.h
@@ -0,0 +1,6 @@
+#ifndef CTRRANDOMIZER_INIT_PROFILE_H
+#define CTRRANDOMIZER_INIT_PROFILE_H
+
+void randomizer_set_profile_defaults(struct AdvProgress* adv);
+
+#endif
---
+++ src/GAMEPROG_05_NewProfile_InsideAdv.c
@@ -0,0 +1,14 @@
+#include <common.h>
+
+/* START Randomizer */
+#include "CTRRandomizer_init_profile.h"
+/* END Randomizer */
+
+void GAMEPROG_NewProfile_InsideAdv(struct AdvProgress* adv)
+{
+    // clear
+    memset(adv, 0x0, sizeof(struct AdvProgress));
+
+    randomizer_set_profile_defaults(adv);
+    /* END Randomizer */
+}
