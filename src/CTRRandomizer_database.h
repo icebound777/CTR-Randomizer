dcf007ec41037985da8959d35f2880ccc109b1ff
Icebound777 <frozenblue@gmx.de>
2025-10-01 22:36:03 +0200

    feat: add warppad unlock db prefixes

--- src/CTRRandomizer_database.h
+++ src/CTRRandomizer_database.h
@@ -4,7 +4,11 @@
 #include <common.h>
 
 /* Key type defines via prefixes */
-#define DB_PREFIX_LEVELIDS 0xA0000000
+enum DbPrefixes {
+    DB_PREFIX_LEVELIDS = 0xA0000000,
+    DB_PREFIX_UNLOCK_ITEMTYPE = 0xA1000000,
+    DB_PREFIX_UNLOCK_ITEMCOUNT = 0xA2000000,
+};
 
 /* DB fetch results */
 enum DbFetchResult {
