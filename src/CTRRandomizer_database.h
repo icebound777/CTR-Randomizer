6397e44e562e636f7d219eac2d91ada8d7617e5f
Icebound777 <frozenblue@gmx.de>
2025-10-10 16:59:19 +0200

    refactor: db prefixes from define to enum

--- src/CTRRandomizer_database.h
+++ src/CTRRandomizer_database.h
@@ -4,7 +4,9 @@
 #include <common.h>
 
 /* Key type defines via prefixes */
-#define DB_PREFIX_LEVELIDS 0xA0000000
+enum DB_Prefixes {
+    DB_PREFIX_LEVELIDS = 0xA0000000
+};
 
 /* DB fetch results */
 enum DbFetchResult {
