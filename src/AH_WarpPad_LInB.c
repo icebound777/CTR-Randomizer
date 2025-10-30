e4ba6e5c2f113fb0392f9261db84189e7655effb
Icebound777 <frozenblue@gmx.de>
2025-10-30 14:53:17 +0100

    style: make warpad linb a bit more readable

--- src/AH_WarpPad_LInB.c
+++ src/AH_WarpPad_LInB.c
@@ -347,12 +347,16 @@ void AH_WarpPad_LInB(struct Instance* inst)
             t->modelIndex = 2;
 
             // if trophy not owned
-            if(CHECK_ADV_BIT(sdata->advProgress.rewards, (levelID + 6)) == 0)
+            if (CHECK_ADV_BIT(sdata->advProgress.rewards, (levelID + 6)) == 0)
             {
                 // open for trophy
                 t->modelIndex = 1;
 
-                newInst = INSTANCE_Birth3D(gGT->modelPtr[STATIC_TROPHY], 0, t);
+                newInst = INSTANCE_Birth3D(
+                    gGT->modelPtr[STATIC_TROPHY],
+                    0,
+                    t
+                );
 
                 newInst->scale[0] = 0x2800;
                 newInst->scale[1] = 0x2800;
@@ -362,16 +366,20 @@ void AH_WarpPad_LInB(struct Instance* inst)
             }
 
             // if token not owned
-            if(CHECK_ADV_BIT(sdata->advProgress.rewards, (levelID + 0x4c)) == 0)
+            if (CHECK_ADV_BIT(sdata->advProgress.rewards, (levelID + 0x4c)) == 0)
             {
                 // not open for trophy
-                if(t->modelIndex != 1)
+                if (t->modelIndex != 1)
                 {
                     // open for relic/token
                     t->modelIndex = 3;
                 }
 BattleTrack:
-                newInst = INSTANCE_Birth3D(gGT->modelPtr[STATIC_TOKEN], 0, t);
+                newInst = INSTANCE_Birth3D(
+                    gGT->modelPtr[STATIC_TOKEN],
+                    0,
+                    t
+                );
 
                 // specular lighting
                 newInst->flags |= 0x30000;
@@ -383,10 +391,11 @@ BattleTrack:
                 i = data.metaDataLEV[levelID].ctrTokenGroupID;
 
                 // token color
-                newInst->colorRGBA =
-                    ((unsigned int)data.AdvCups[i].color[0] << 0x14) |
-                    ((unsigned int)data.AdvCups[i].color[1] << 0xc) |
-                    ((unsigned int)data.AdvCups[i].color[2] << 0x4);
+                newInst->colorRGBA = (
+                    ((unsigned int)data.AdvCups[i].color[0] << 0x14)
+                    | ((unsigned int)data.AdvCups[i].color[1] << 0xc)
+                    | ((unsigned int)data.AdvCups[i].color[2] << 0x4)
+                );
 
                 // === Naughty Dog Bug ===
                 // They made an array where every token color
@@ -408,18 +417,25 @@ BattleTrack:
 SlideColTurboTrack:
 
             // if relic not owned
-            if(levelID < NITRO_COURT) // check this cause of "goto BattleTrack"
-            if(CHECK_ADV_BIT(sdata->advProgress.rewards, (levelID + 0x16)) == 0)
+            if (levelID < NITRO_COURT) // check this cause of "goto BattleTrack"
+            if (CHECK_ADV_BIT(sdata->advProgress.rewards, (levelID + 0x16)) == 0)
             {
                 // SlideCol/TurboTrack
-                if(levelID>=SLIDE_COLISEUM)
+                if (levelID >= SLIDE_COLISEUM)
+                {
                     t->modelIndex = 4;
-
-                // open for token/relic
-                else if(t->modelIndex != 1)
+                }
+                else if (t->modelIndex != 1)
+                {
+                    // open for token/relic
                     t->modelIndex = 3;
+                }
 
-                newInst = INSTANCE_Birth3D(gGT->modelPtr[STATIC_RELIC], 0, t);
+                newInst = INSTANCE_Birth3D(
+                    gGT->modelPtr[STATIC_RELIC],
+                    0,
+                    t
+                );
 
                 // relic blue
                 newInst->colorRGBA = 0x20a5ff0;
@@ -434,11 +450,11 @@ SlideColTurboTrack:
                 warppadObj->inst[WPIS_OPEN_PRIZE3] = newInst;
             }
 
-            for(i = 0; i < 3; i++)
+            for (i = 0; i < 3; i++)
             {
-                newInst = warppadObj->inst[WPIS_OPEN_PRIZE1+i];
+                newInst = warppadObj->inst[WPIS_OPEN_PRIZE1 + i];
 
-                if(newInst == 0) continue;
+                if (newInst == 0) continue;
 
                 // copy matrix
                 *(int*)((int)&newInst->matrix + 0x0) = *(int*)((int)&inst->matrix + 0x0);
@@ -451,25 +467,21 @@ SlideColTurboTrack:
                 newInst->matrix.t[2] = inst->matrix.t[2];
             }
         }
-
-        // slide col, turbo track
-        else if(levelID < NITRO_COURT)
+        else if(levelID < NITRO_COURT) // slide col, turbo track
         {
             // already unlocked
             t->modelIndex = 2;
 
             goto SlideColTurboTrack;
         }
-
-        // battle tracks
-        else if(levelID < GEM_STONE_VALLEY)
+        else if (levelID < GEM_STONE_VALLEY) // battle tracks
         {
             i = R232.battleTrackArr[levelID - NITRO_COURT] + 0x6f;
 
             // already unlocked
             t->modelIndex = 2;
 
-            if(CHECK_ADV_BIT(sdata->advProgress.rewards, i) == 0)
+            if (CHECK_ADV_BIT(sdata->advProgress.rewards, i) == 0)
             {
                 // rainbow
                 t->modelIndex = 4;
@@ -477,15 +489,13 @@ SlideColTurboTrack:
                 goto BattleTrack;
             }
         }
-
-        // gemstone valley
-        else
+        else // gemstone valley
         {
             // bit index of gem
             i = (levelID - ADV_CUP) + 0x6a;
 
             // if gem is already unlocked, quit
-            if(CHECK_ADV_BIT(sdata->advProgress.rewards, i) != 0)
+            if (CHECK_ADV_BIT(sdata->advProgress.rewards, i) != 0)
             {
                 // beaten
                 t->modelIndex = 2;
@@ -496,7 +506,11 @@ SlideColTurboTrack:
             // rainbow color
             t->modelIndex = 4;
 
-            newInst = INSTANCE_Birth3D(gGT->modelPtr[STATIC_GEM], 0, t);
+            newInst = INSTANCE_Birth3D(
+                gGT->modelPtr[STATIC_GEM],
+                0,
+                t
+            );
 
             // specular lighting
             newInst->flags |= 0x20000;
@@ -504,10 +518,11 @@ SlideColTurboTrack:
             i = levelID - ADV_CUP;
 
             // token color
-            newInst->colorRGBA =
-                ((unsigned int)data.AdvCups[i].color[0] << 0x14) |
-                ((unsigned int)data.AdvCups[i].color[1] << 0xc) |
-                ((unsigned int)data.AdvCups[i].color[2] << 0x4);
+            newInst->colorRGBA = (
+                ((unsigned int)data.AdvCups[i].color[0] << 0x14)
+                | ((unsigned int)data.AdvCups[i].color[1] << 0xc)
+                | ((unsigned int)data.AdvCups[i].color[2] << 0x4)
+            );
 
             warppadObj->inst[WPIS_OPEN_PRIZE1] = newInst;
 
@@ -527,12 +542,11 @@ SlideColTurboTrack:
 
     // === if locked ===
 
-    if(unlockItem_numNeeded < 10)
+    if (unlockItem_numNeeded < 10)
     {
         warppadObj->digit10s = 0;
         warppadObj->digit1s = unlockItem_numNeeded;
     }
-
     else
     {
         warppadObj->digit10s = 1;
