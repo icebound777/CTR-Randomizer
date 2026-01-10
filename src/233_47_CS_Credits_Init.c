b93c2bc88c4aa4e34a66eba219e9eca3bb2fe306
Icebound777 <frozenblue@gmx.de>
2026-01-10 19:01:05 +0100

    style: format CS_Credits_Init

--- src/233_47_CS_Credits_Init.c
+++ src/233_47_CS_Credits_Init.c
@@ -9,24 +9,24 @@ void CS_Credits_Init()
 {
     int i;
     int bitIndex;
-    struct Instance* inst;
+    struct Instance *inst;
 
     // optimization
     //int boolAllBlue;
     #define boolAllBlue creditsBSS->boolAllBlue
 
     int boolAllGold;
-    struct GameTracker* gGT;
-    struct AdvProgress* advProg;
-    struct CreditsObj* creditsObj;
-    struct CreditsLevHeader* CLH;
-    struct CreditsLevHeader* creditsDst;
+    struct GameTracker *gGT;
+    struct AdvProgress *advProg;
+    struct CreditsObj *creditsObj;
+    struct CreditsLevHeader *CLH;
+    struct CreditsLevHeader *creditsDst;
 
     gGT = sdata->gGT;
     advProg = &sdata->advProgress;
     creditsObj = &creditsBSS->creditsObj;
 
-    void** pointers = ST1_GETPOINTERS(gGT->level1->ptrSpawnType1);
+    void **pointers = ST1_GETPOINTERS(gGT->level1->ptrSpawnType1);
     CLH = pointers[ST1_CREDITS];
 
     creditsBSS->DancerThread = 0;
@@ -34,7 +34,7 @@ void CS_Credits_Init()
     boolAllBlue = 1;
     boolAllGold = 1;
 
-    for(i = 0; i < 0x12; i++)
+    for (i = 0; i < 0x12; i++)
     {
         if (boolAllBlue != 0)
         {
@@ -51,7 +51,7 @@ void CS_Credits_Init()
 
     #undef boolAllBlue
 
-    if(boolAllGold != 0)
+    if (boolAllGold != 0)
     {
         gGT->numWinners = 1;
         gGT->winnerIndex[0] = 0;
@@ -64,14 +64,18 @@ void CS_Credits_Init()
     // 0 = no relation to param4
     // 0x300 = SmallStackPool
     // 0xd = "other" thread bucket
-    creditsBSS->CreditThread =
-        PROC_BirthWithObject(0x30d, CS_Credits_ThTick, NULL, NULL);
+    creditsBSS->CreditThread = PROC_BirthWithObject(
+        0x30d,
+        CS_Credits_ThTick,
+        NULL,
+        NULL
+    );
 
     memset(creditsObj, 0, sizeof(struct CreditsObj));
     creditsObj->countdown = FPS_DOUBLE(360);
 
     // === 5 instances ===
-    for(i = 0; i < 5; i++)
+    for (i = 0; i < 5; i++)
     {
         #if 0
         // OG game passes CreditsThread as parameter,
@@ -106,13 +110,12 @@ void CS_Credits_Init()
 
     creditsBSS->numStrings = creditsDst->numStrings;
 
-    char** ptrStrings = (char**)CREDITSHEADER_GETSTRINGS(creditsDst);
+    char **ptrStrings = (char**) CREDITSHEADER_GETSTRINGS(creditsDst);
     creditsBSS->ptrStrings = ptrStrings;
 
-    for(i = 0; i < creditsBSS->numStrings; i++)
+    for (i = 0; i < creditsBSS->numStrings; i++)
     {
-        ptrStrings[i] =
-        (char*)((unsigned int)ptrStrings[i] + (unsigned int)creditsDst);
+        ptrStrings[i] = (char*) ((unsigned int)ptrStrings[i] + (unsigned int)creditsDst);
     }
 
     creditsObj->credits_posY = 340;
